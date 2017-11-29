1a4a47b443ecd33029c8ae1596c991008d1f0c2f
ChrisBusch <Chris.Busch@gmail.com>
2017-11-29 13:48:08 -0600

    md upload

---
+++ generate-mds.sh
@@ -0,0 +1,9 @@
+##rem pandoc --extract-media ./myMediaFolder input.docx -o output.md
+
+pandoc --extract-media ./img-readme SocialDeterminatesofMortality.docx -o readme.md
+pandoc --extract-media ./img-modeling2 modeling2.docx -o modeling2.md
+pandoc --extract-media ./img-process-chr-files process-chr-files.docx -o process-chr-files.md
+pandoc --extract-media ./img-process-irs-files process-irs-files.docx -o process-irs-files.md
+pandoc --extract-media ./img-process-wonder-files process-wonder-files.docx -o process-wonder-files.md
+
+
